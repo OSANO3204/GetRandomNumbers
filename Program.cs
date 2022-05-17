514b795ab900de545df8d0c23bf046e9e20afdc2
brian.otieno@ngaocredit.com <brian.otieno@ngaocredit.com>
2022-05-17 12:29:14 +0300

    GetRandomNumbers

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program getrandomnumbers = new Program();
+            var getAllharsh = getrandomnumbers.getRandom();
+            Console.WriteLine($"The harshed numbers are {getAllharsh}" );
+        }
+      
+
+        public int getRandom()
+        {
+           var  getrandom = new Random().Next(1000000, 90000089).GetHashCode();
+
+            return getrandom;
+        }
+
+    }
+}
